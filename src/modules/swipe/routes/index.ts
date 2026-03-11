07fdcd4be86e892df4ac36add69cc4ddcf5df201
Lonnie <iamadigga@gmail.com>
2026-03-11 19:05:51 -0400

    Fix swipe router mounting

--- src/modules/swipe/routes/index.ts
+++ src/modules/swipe/routes/index.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
+import swipeRouter from "./swipe";
 
 const router = Router();
 
-router.get("/", (req, res) => {
+/* Health check */
+
+router.get("/", (_req, res) => {
   res.json({ message: "Swipe routes working" });
 });
+
+/* Actual swipe routes */
+
+router.use("/", swipeRouter);
 
 export default router;
